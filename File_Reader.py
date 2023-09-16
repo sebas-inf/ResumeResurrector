fa07bd8c2d1386031a164fa632d6d52adf278d30
Yuinyan20 <6dinformaticasinfante@gmail.com>
2023-09-16 14:09:14 -0400

    commi

--- File_Reader.py
+++ File_Reader.py
@@ -12,18 +12,18 @@ import my_key
 
 
 def read_resume(jd, resume, options):
-    df = analyze_str(resume, options)
-    df_string = df.applymap(lambda x: ', '.join(x) if isinstance(x, list) else x).to_string(index=False)
+    new_resume = analyze_str(resume, options)
+    resume_string = new_resume.applymap(lambda x: ', '.join(x) if isinstance(x, list) else x).to_string(index=False)
     st.write("OpenAI comprehensive analysis..")
-    summary_question = f"Job requirements: {{{jd}}}" + f"Resume summary: {{{df_string}}}" + ", please directly provide the candidate's match summary for this position (within 200 words);'"
+    summary_question = f"Job requirements: {{{jd}}}" + f"Resume summary: {{{resume_string}}}" + ", please directly provide the candidate's match summary for this position (within 200 words);'"
     summary = ask_openAI(summary_question)
-    df.loc[len(df)] = ['Comprehensive summary', summary]
+    new_resume.loc[len(new_resume)] = ['Comprehensive summary', summary]
     extra_info = "Scoring requirements: top 10 universities in China +3 points, 985 universities +2 points, 211 universities +1 point, top company experience +2 points, well-known company +1 point, overseas background +3 points, foreign company background +1 point. "
-    score_question = f"Job requirements: {{{jd}}}" + f"Resume summary: {{{df.to_string(index=False)}}}" + ", please directly return the candidate's match score for this job (0-100), please score accurately for easy comparison and ranking for other candidates, '" + extra_info
+    score_question = f"Job requirements: {{{jd}}}" + f"Resume summary: {{{new_resume.to_string(index=False)}}}" + ", please directly return the candidate's match score for this job (0-100), please score accurately for easy comparison and ranking for other candidates, '" + extra_info
     score = ask_openAI(score_question)
-    df.loc[len(df)] = ['Match score', score]
+    new_resume.loc[len(new_resume)] = ['Match score', score]
 
-    return resume
+    return new_resume
 
 def analyze_str(resume, options):
     text_splitter = CharacterTextSplitter(
@@ -37,7 +37,7 @@ def analyze_str(resume, options):
     embeddings = OpenAIEmbeddings(openai_api_key=my_key.get_key())
     knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-    df_data = [{'option': option, 'value': []} for option in options]
+    resume_data = [{'option': option, 'value': []} for option in options]
     st.write("Information Retrieval")
 
     # Create progress bar and empty element
@@ -50,13 +50,24 @@ def analyze_str(resume, options):
         llm = OpenAI(openai_api_key=my_key.get_key(), temperature=0.3, model_name="text-davinci-003", max_tokens="2000")
         chain = load_qa_chain(llm, chain_type="stuff")
         response = chain.run(input_documents=docs, question=question )
-        df_data[i]['value'] = response
+        resume_data[i]['value'] = response
         option_status.text(f"Fetching information for: {option}")
 
         # Update progress bar
         progress = (i + 1) / len(options)
         progress_bar.progress(progress)
 
-    df = pd.DataFrame(df_data)
+    df = pd.DataFrame(resume_data)
     st.success("Resume elements retrieved")
     return df
+
+def ask_openAI(question):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=question,
+        max_tokens=400,
+        n=1,
+        stop=None,
+        temperature=0,
+    )
+    return response.choices[0].text.strip()
