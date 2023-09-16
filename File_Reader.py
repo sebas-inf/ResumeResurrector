730fe62da3575bf3a8bddbe3daa43d7088c8d068
TripleJ160 <83926918+TripleJ160@users.noreply.github.com>
2023-09-16 15:05:03 -0400

    Latest implementation with backup uploaded

--- File_Reader.py
+++ File_Reader.py
@@ -7,7 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import OpenAI
-import my_key
+#import my_key
 
 
 
